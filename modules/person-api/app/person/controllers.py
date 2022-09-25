f37be29cc29dc12ec3df70a69d87d6d0f12c82b0
Mahmoud Abdallah <mah64725@gmail.com>
2022-09-25 19:45:14 +0200

    finish p2

--- modules/person-api/app/person/controllers.py
+++ modules/person-api/app/person/controllers.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 
-from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import (
+from app.person.models import Connection, Person
+from app.person.schemas import (
     ConnectionSchema,
-    LocationSchema,
     PersonSchema,
 )
-from app.udaconnect.services import ConnectionService, LocationService, PersonService
+from app.person.services import ConnectionService, PersonService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
@@ -14,27 +13,8 @@ from typing import Optional, List
 
 DATE_FORMAT = "%Y-%m-%d"
 
-api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
+api = Namespace("Person", description="Connections via geolocation.")  # noqa
 
-
-# TODO: This needs better exception handling
-
-
-@api.route("/locations")
-@api.route("/locations/<location_id>")
-@api.param("location_id", "Unique ID for a given Location", _in="query")
-class LocationResource(Resource):
-    @accepts(schema=LocationSchema)
-    @responds(schema=LocationSchema)
-    def post(self) -> Location:
-        request.get_json()
-        location: Location = LocationService.create(request.get_json())
-        return location
-
-    @responds(schema=LocationSchema)
-    def get(self, location_id) -> Location:
-        location: Location = LocationService.retrieve(location_id)
-        return location
 
 
 @api.route("/persons")
@@ -67,7 +47,7 @@ class PersonResource(Resource):
 @api.param("distance", "Proximity to a given user in meters", _in="query")
 class ConnectionDataResource(Resource):
     @responds(schema=ConnectionSchema, many=True)
-    def get(self, person_id) -> ConnectionSchema:
+    def get(self, person_id) -> List[Connection]:
         start_date: datetime = datetime.strptime(
             request.args["start_date"], DATE_FORMAT
         )
